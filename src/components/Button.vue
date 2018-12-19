85ef39892da382a2b9760c02a7fa9fc6b27df872
Brenda Sanrey <sanrey254@hotmail.com>
2018-12-19 15:57:03 -0600

    adding changes

---
+++ src/components/Button.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <a class="button has-text-white has-background-report is-report">
+      <span class="icon">
+        <i class="fas fa-ban"></i>
+      </span>
+      <span>Cancelar</span>
+    </a>
+    <a
+      class="button has-text-white has-background-income is-income"
+      :class="{'disabled': isDisabled}"
+    >
+      <span class="icon">
+        <i class="fas fa-arrow-circle-right"></i>
+      </span>
+      <span>Enviar</span>
+    </a>
+  </div>
+</template>
+<script>
+export default {
+  name: "Button",
+  data() {
+    return {
+      isDisabled: true
+    };
+  },
+  mounted() {
+    this.$root.$on("ActiveButton", lengthName => {
+      if (lengthName < 5) {
+        this.isDisabled = true;
+      } else {
+        this.isDisabled = false;
+      }
+    });
+  }
+};
+</script>
--- src/components/__tests__/SomeComponent.test.js
+++ src/components/__tests__/SomeComponent.test.js
@@ -1,5 +1,59 @@
-describe("SomeComponent.vue", () => {
-  it("works", () => {
-    expect(true).toBe(true);
+import { mount } from "@vue/test-utils";
+import Buttons from "@/components/Button.vue";
+import Input from "@/components/Input.vue";
+import Main from "@/App.vue";
+
+describe("Buttons.vue", () => {
+  const wrapper = mount(Buttons);
+
+  test("there are two buttons", () => {
+    const buttons = wrapper.findAll(".button");
+    expect(buttons.length).toBe(2);
+  });
+
+  test("the button is disabled by default", () => {
+    const button = wrapper.find("a:last-child");
+    expect(button.classes("disabled")).toBe(true);
+  });
+});
+
+describe("Input.vue", () => {
+  test("Renders props message when passed", () => {
+    const msg = "defaul message";
+    const wrapper = mount(Input, { propsData: { msg } });
+    expect(wrapper.find("h1").text()).toBe(msg);
+  });
+
+  test("Classes change when name is ", () => {
+    const wrapper = mount(Input);
+    const input = wrapper.find("input");
+    expect(input.is("input")).toBe(true);
+    input.setValue("Palmidia");
+    expect(input.classes("error")).toBe(true);
+    input.setValue("Montserratt");
+    expect(input.classes("valid")).toBe(true);
+  });
+
+  test("Emitted event active button", () => {
+    const wrapper = mount(Input);
+    const name = "mario";
+    wrapper.vm.$emit("ActiveButton", name.length);
+    expect(wrapper.emitted().ActiveButton).toBeTruthy();
+  });
+});
+
+describe("App.vue", () => {
+  const wrapper = mount(Main);
+  test("Renders children", () => {
+    const children = wrapper.vm.$children;
+    expect(children.length).toBe(2);
+  });
+
+  test("button change to active when the input is valid", () => {
+    const inputChild = wrapper.find(Input);
+    const button = wrapper.find("a:last-child");
+    expect(button.classes("disabled")).toBe(true);
+    inputChild.vm.$emit("ActiveButton");
+    expect(button.classes()).not.toBe("disabled");
   });
 });
