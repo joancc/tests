bc7fb0482e17591660a0074ab59d49f2650ee651
jetzable <gjetzable@gmail.com>
2018-12-20 00:01:19 -0600

    Adding Companies Column as Component

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,33 @@
 <template>
-  <div id="app">
-    <div>
-      <Input msg="Welcome to Your Vue.js App"/>
-      <SomeComponent/>
-    </div>
-  </div>
+	<div id="app">
+		<div>
+			<Input msg="Welcome to Your Vue.js App"/>
+			<SomeComponent/>
+		</div>
+	</div>
 </template>
 
 <script>
-import SomeComponent from "./components/SomeComponent.vue";
-import Input from "./components/Input.vue";
+	import SomeComponent from "./components/SomeComponent.vue";
+	import Input from "./components/Input.vue";
 
-export default {
-  name: "app",
-  components: {
-    SomeComponent,
-    Input
-  }
-};
+	export default {
+		name: "app",
+		components: {
+			SomeComponent,
+			Input
+		}
+	};
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	#app {
+		font-family: "Avenir", Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+		padding: 2em;
+	}
 </style>
