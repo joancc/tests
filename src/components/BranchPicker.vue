8572eb2b80806c25ed5bb809821ab9c12ed89922
jetzable <gjetzable@gmail.com>
2019-01-04 15:35:38 -0600

    Adding test for companies and branch

--- src/components/BranchPicker.vue
+++ src/components/BranchPicker.vue
@@ -1,45 +1,45 @@
 <template>
-	<div class="select-branch" style="border: 2px solid purple;">
-		<div class="outer-20-b">
-			<a @click="handleCloseColumns">
-				<i class="far fa-times-circle"></i> Cerrar
-			</a>
-		</div>
-		<div>
-			<CompanyList class="is-one-third"/>
-			<StoreList :class="{'is-hidden': showStoreColumn}" class="is-one-third"/>
-			<LocationList :class="{'is-hidden': showLocationColumn}" class="is-one-third"/>
-		</div>
-	</div>
+  <div class="select-branch" style="border: 2px solid purple;">
+    <div class="outer-20-b">
+      <a @click="handleCloseColumns">
+        <i class="far fa-times-circle"></i> Cerrar
+      </a>
+    </div>
+    <div>
+      <CompanyList class="is-one-third"/>
+      <StoreList :class="{'is-hidden': showStoreColumn}" class="is-one-third"/>
+      <!-- <LocationList :class="{'is-hidden': showLocationColumn}" class="is-one-third"/> -->
+    </div>
+  </div>
 </template>
 <script>
-	import { mapGetters } from "vuex";
-	import CompanyList from "./CompanyList.vue";
-	import StoreList from "./StoreList.vue";
-	import LocationList from "./LocationList.vue";
-	import { log } from "util";
+import { mapGetters } from "vuex";
+import CompanyList from "./CompanyList.vue";
+import StoreList from "./StoreList.vue";
+import LocationList from "./LocationList.vue";
+import { log } from "util";
 
-	export default {
-		name: "BranchPicker",
-		computed: {
-			...mapGetters(["showStoreColumn", "showLocationColumn"])
-		},
-		data() {
-			return {
-				closeColumns: false
-			};
-		},
-		methods: {
-			handleCloseColumns() {
-				this.closeColumns = !this.closeColumns;
-			}
-		},
-		components: {
-			CompanyList,
-			StoreList,
-			LocationList
-		}
-	};
+export default {
+  name: "BranchPicker",
+  computed: {
+    ...mapGetters(["showStoreColumn", "showLocationColumn"])
+  },
+  data() {
+    return {
+      closeColumns: false
+    };
+  },
+  methods: {
+    handleCloseColumns() {
+      this.closeColumns = !this.closeColumns;
+    }
+  },
+  components: {
+    CompanyList,
+    StoreList,
+    LocationList
+  }
+};
 </script>
 
 		<div class="columns" :class="{'is-hidden': closeColumns}">
---
+++ src/components/__tests__/CompanyList.test.js
@@ -0,0 +1,95 @@
+import {
+  createLocalVue,
+  mount
+} from "@vue/test-utils";
+import {
+  getQueriesForElement,
+  prettyDOM,
+  wait,
+  fireEvent
+} from "dom-testing-library";
+import Vuex from "vuex";
+import BranchPicker from '../BranchPicker.vue';
+import CompanyList from "../CompanyList.vue";
+function render(component, options, storeOptions) {
+  const localVue = createLocalVue();
+  localVue.use(Vuex);
+
+  const store = new Vuex.Store(storeOptions);
+
+  const wrapper = mount(component, {
+    localVue,
+    attachToDocument: true,
+    ...options,
+    store
+  });
+
+  return {
+    wrapper,
+    ...getQueriesForElement(wrapper.element),
+    debug: () => console.log(prettyDOM(wrapper.element))
+  };
+}
+describe("CompanyList", () => {
+  const branches = [
+    {
+      branch_id: 144,
+      type: "Store",
+      name: "San Juan",
+      active: true
+    },
+    {
+      branch_id: 4687,
+      type: "Shop",
+      name: "Fer_Pruebas_test",
+      active: true
+    },
+    {
+      branch_id: 4706,
+      type: "Shop",
+      name: "David Test",
+      active: true
+    },
+    {
+      branch_id: 145,
+      type: "Shop",
+      name: "Coyoacán",
+      active: true
+    }
+  ];
+  let getters = {
+    companiesFromServer: () => [
+      {
+        company_id: 17,
+        status: true,
+        emitter: {
+          id: 11,
+          tax_id: "JAR1106038RA",
+          business_name: "Soluciones Eléctricas",
+          commercial_name: "Soluciones Eléctricas Ibarra Updated S.A. de C.V."
+        }
+      }
+    ],
+    branchList: () => branches,
+    showStoreColumn: () => true
+  };
+  let actions = {
+    getCompaniesFromServer: jest.fn(),
+    handleCompanySelect: jest.fn(id => id)
+  };
+
+  let storeOptions = {
+    getters,
+    actions
+  };
+  test("calls store action getCompaniesFromServer when the component is rendered", async () => {
+    const { wrapper, getByText } = render(CompanyList, {}, storeOptions);
+    expect(getByText("Soluciones Eléctricas")).toBeTruthy();
+  });
+  test("it renders branches according to selection", () => {
+    const { wrapper, getByText, debug } = render(BranchPicker, {}, storeOptions);
+    const company = wrapper.find('button');
+    company.trigger('click');
+    expect(getByText(branches[1].name)).toBeTruthy();
+  });
+});
